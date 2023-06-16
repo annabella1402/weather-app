f1bbdb903f28b99630f675dbaad0dc4e5d8aa6b8
annabella1402 <136617763+annabella1402@users.noreply.github.com>
2023-06-16 11:13:25 -0400

    Created the CSS and JS files

---
+++ script.js
@@ -0,0 +1,137 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20
+  }
+};
+
+let city = prompt("Enter a city?");
+
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let humidity = weather[city].humidity;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+  alert(
+    `It is currently ${celsiusTemperature} (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
+
+function displayCurrentTime() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let currentTime = document.querySelector("#current-time");
+  currentTime.innerHTML = `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let h1 = document.querySelector("h1");
+  let cityInput = document.querySelector("#city-input");
+  cityInput = cityInput.value.trim();
+  h1.innerHTML = `${cityInput}`;
+}
+
+let cityForm = document.querySelector("form");
+cityForm.addEventListener("submit", search);
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+displayCurrentTime();
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "b2d9fa1f2b35557e4615dd5fab218834";
+  let city = document.querySelector("#search-bar").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+let units = "metric";
+
+function showWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#precipitation").innerHTML =
+    response.data.main.precipitation;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function getCurrentPosition(position) {
+  let apiKey = "09f15a0880bebf0e4507d2f1b4462892";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function locationTemperature(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCurrentPosition);
+}
+
+let currentButton = document.querySelector("#current-location-button");
+currentButton.addEventListener("click", locationTemperature);
